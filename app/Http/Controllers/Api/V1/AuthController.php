c372a9563c53485f760b7d1b076d041b7a9f08ae
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-06-26 15:27:47 +0300

    Registartion, Authorization and Tokens

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(StoreUserRequest $request)
+    {
+        return User::create($request->all());
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        if (! Auth::attempt($request->only(['email', 'password']))) {
+            return response()->json([
+                'message'=> 'Sorry, those credentials do not match.',
+            ], 401);
+        }
+
+        // $user = Auth::user();
+        $user = User::query()->where('email', $request->email)->first();
+
+        $user->tokens()->delete();
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken("Token for user: {$user->name}")->plainTextToken
+        ]);
+    }
+
+    public function logout()
+    {
+        // Auth::logout();
+        return 'logout';
+    }
+}
